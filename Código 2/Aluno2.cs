f5d4a60b03879eaa3d806887e8a354b98087c964
DKAT-DAVI <eudkatdavikalel@gmail.com>
2022-06-24 21:57:57 -0300

    Get e set diferentes e números float

---
+++ Código 2/Aluno2.cs
@@ -0,0 +1,7 @@
+using System;
+
+public class Aluno2
+{
+    //Outro modo de usar o get e o set
+    public String Nome { get; set; }        
+}
--- Código 2/Program.cs
+++ Código 2/Program.cs
@@ -8,16 +8,36 @@ namespace Program
         {
             //Definição de um novo objeto do tipo Aluno.
             Aluno aluno = new Aluno();
+            Aluno aluno2 = new Aluno();
+            Aluno2 aluno3 = new Aluno2();
+            Aluno2 aluno4 = new Aluno2();
 
             //Receber o nomeAluno.
             aluno.SetNome("Davi Kalel");
+            aluno2.SetNome("Pedro Isaque");
+            aluno3.Nome = ("Davi Kalel");
+            aluno4.Nome = ("Pedro Isaque");
 
             //Devolver o NomeAluno inserido.
             Console.WriteLine($"Nome do aluno: {aluno.GetNome()}");
+            Console.WriteLine($"Nome do aluno2: {aluno2.GetNome()}");
+            Console.WriteLine($"Nome do aluno3: {aluno3.Nome}");
+            Console.WriteLine($"Nome do aluno4: {aluno4.Nome}");
 
-            Aluno aluno2 = new Aluno();
-            aluno2.SetNome("Pedro Isaque");
-            Console.WriteLine($"Nome do aluno: {aluno2.GetNome()}");
+            //Numbers Float
+            
+            //Single ou float -> não é preciso.
+            Console.WriteLine(".7f = {0:F5}", .7f);
+            Console.WriteLine(".7f = {0:F35}", .7f);
+            
+            //Double_-> não é preciso, mais preciso que float.
+            Console.WriteLine(".7d = {0:F35}", .7);
+
+            //Decimal -> absolutamente preciso, consome mais memória e processamento.
+            Console.WriteLine(".7m = {0:F35}", .7m);
+
+            //Não preciso.
+            Console.WriteLine("84.05d - 84.04d = {0:F35}", 84.05 - 84.04);
         }
     }
 }

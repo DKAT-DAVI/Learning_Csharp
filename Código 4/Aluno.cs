3e194c1913c9c1ced272f627575f794c94cfe3fe
DKAT-DAVI <eudkatdavikalel@gmail.com>
2022-06-26 22:05:16 -0300

    Construtores

---
+++ Código 4/Aluno.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Aluno
+{
+    public String Nome { get; set;}
+    //Construtor padrão
+    public Aluno()
+    {
+        //Declarando construtor padrão como "Sem nome definido"
+        Nome = "Sem nome definido";
+    }
+}
--- Código 4/Program.cs
+++ Código 4/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)        
+        {
+            Aluno aluno = new Aluno();
+            Console.WriteLine($"Nome: {aluno.Nome}");
+        }
+    }
+}

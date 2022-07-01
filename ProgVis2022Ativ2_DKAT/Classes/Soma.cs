7b0015adef8ba70e02956eb325a91c93c81d36da
DKAT-DAVI <eudkatdavikalel@gmail.com>
2022-07-01 18:44:24 -0300

    Implementação da classe Soma e do método Cabecalho

--- ProgVis2022Ativ2_DKAT/Classes/Soma.cs
+++ ProgVis2022Ativ2_DKAT/Classes/Soma.cs
@@ -1,6 +1,32 @@
 using System;
 
+//Classe que faz a soma de dois números
 public class Soma
 {
+    //Atributos
+    /*Atributo ValorA:
+    Recebe o valor do primeiro número
+    para efetuar a soma.*/
+    public Double ValorA { get; set;}  
+
+    /*Atributo ValorB:
+    Recebe o valor do segundo número
+    para efetuar a soma.*/
+    public Double ValorB { get; set;}   
+
+    /*Método Operar:
+    Opera a soma retornando o resultado
+    de ValorA + ValorB.*/
+    public Double Operar()
+    {
+        return ValorA + ValorB;
+    }
+
+    /*Formatação da exibição, usando o
+    método ToString.*/
+    public override string ToString()
+    {
+        return $"Valor A = {ValorA}\nValor B = {ValorB}\nSoma = {Operar()}";
+    }
 
 }
--- ProgVis2022Ativ2_DKAT/Program.cs
+++ ProgVis2022Ativ2_DKAT/Program.cs
@@ -11,6 +11,13 @@ namespace Program{
             ProcessarQuestao5();
         }
         
+        //Método para exibição de cabeçalho da questão.
+        private static void Cabeçalho(int numeroDaQuestao) {
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine($"|      Resposta da Questão {numeroDaQuestao}      |");
+            Console.WriteLine("-----------------------------------\n");
+        }
+
         //Métodos para a resolução das questões.
         private static void ProcessarQuestao1() {
             
@@ -22,6 +29,11 @@ namespace Program{
             
         }
         private static void ProcessarQuestao4() {
+            Cabeçalho(4);
+            Soma soma = new Soma();
+            soma.ValorA = 2.5;
+            soma.ValorB = -3.0;
+            Console.WriteLine($"Objeto: Soma\nToString():\n{soma}");
 
         }
         private static void ProcessarQuestao5() {

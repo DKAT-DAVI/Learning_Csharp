59c9b6c4b165409588981a0d404fa4f5a6b158bf
DKAT-DAVI <eudkatdavikalel@gmail.com>
2022-07-01 18:14:36 -0300

    Criação dos arquivos das classes

--- ProgVis2022Ativ2_DKAT/Program.cs
+++ ProgVis2022Ativ2_DKAT/Program.cs
@@ -1,9 +1,31 @@
 ﻿using System;
 
-namespace Program
-{
-    public class Program
-    {
+namespace Program{
+    class Program {
+        public static void Main(string[] args) {
+            //Invocação de todos os métodos de resolução das questões.
+            ProcessarQuestao1();
+            ProcessarQuestao2();
+            ProcessarQuestao3();
+            ProcessarQuestao4();
+            ProcessarQuestao5();
+        }
         
+        //Métodos para a resolução das questões.
+        private static void ProcessarQuestao1() {
+            
+        }
+        private static void ProcessarQuestao2() {
+            
+        }
+        private static void ProcessarQuestao3() {
+            
+        }
+        private static void ProcessarQuestao4() {
+
+        }
+        private static void ProcessarQuestao5() {
+            
+        }
     }
 }

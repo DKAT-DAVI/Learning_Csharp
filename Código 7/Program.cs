9e2482bfbd25dac795350d982bfcb707ba69f4af
DKAT-DAVI <eudkatdavikalel@gmail.com>
2022-07-02 11:50:55 -0300

    Listas fortemente tipadas

--- Código 7/Program.cs
+++ Código 7/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Program 
 {
@@ -56,6 +57,55 @@ namespace Program
             foreach(Discente discente in turmaA) 
             {
                 Console.WriteLine($"Discente: {discente.Nome}");
+            }
+
+            Console.WriteLine();
+
+            Discente d2 = new Discente("Kalel");
+            Discente d3 = new Discente("Pedro");
+            
+            //List<T>
+            /*Lista fortemente tipada de objetos que podem
+            ser acessados por meio de um índice.*/
+            
+            //List<data type> nomeDaVariavel = new List<data type>();
+            List<Discente> turmaB = new List<Discente>();
+
+            //Métodos.
+            //Inserção de dados.
+            //A Lista permite repetição de dados.
+            turmaB.Add(d1);
+            turmaB.Add(d2);
+            turmaB.Add(d3);
+            turmaB.Add(d3);
+            turmaB.Add(new Discente() {Nome = "Matheus"});
+
+            //Busca de dados.
+            bool contem = turmaB.Contains(d3);
+            Console.WriteLine(contem);
+
+            //Retorna índice do objeto especificado.
+            int indice = turmaB.IndexOf(d3);
+            Console.WriteLine(indice);
+
+            //Remover dado.
+            turmaB.Remove(d1);
+
+            //Remover dado em posição especificada.
+            turmaB.RemoveAt(1);
+
+            //Inserir dado em posição especificada.
+            turmaB.Insert(1, d3);
+
+            //Ordenar lista.
+            //turmaB.Sort();
+
+            //Limpar lista.
+            turmaB.Clear();
+
+            foreach(Discente nome in turmaB)
+            {
+                Console.WriteLine($"Discente: {nome.Nome}");
             }
         }
     }
